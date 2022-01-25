e890096f20fb2f71be1d3632f778fd26aee951a0
MrTSCode <taurussilvercode@gmail.com>
2022-01-25 14:06:52 +0700

    tes push dari hosting

---
+++ host.php
@@ -0,0 +1,16 @@
+<?php
+var_dump(gethostname().' '.$_SERVER['HTTP_HOST']);
+
+var_dump($_SERVER['SERVER_PROTOCOL']);
+
+if (isset($_SERVER['HTTPS']) &&
+    ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+    isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+    $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+  $protocol = 'https://';
+}
+else {
+  $protocol = 'http://';
+}
+
+echo $protocol;
